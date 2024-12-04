19e84069fc7dc5493f7f42bf4558d7a0c403a564
moon777 <myj2139@gmail.com>
2024-12-04 16:11:08 +0900

    [문영주] - memo 로그인, 회원가입 프레임 위치 조정

--- src/main/java/com/bbar/memo/user/UserRestController.java
+++ src/main/java/com/bbar/memo/user/UserRestController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bbar.memo.user.domain.User;
